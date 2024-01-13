ffd3846d8083ea4891adfdece2208c315e960799
Luke_YS <yjsmk0902@gmail.com>
2024-01-13 12:08:51 +0900

    :stethoscope: Chore: HealthCheck API Swagger 명세 수정
    @Hidden을 추가하여 Swagger 명세를 하지 않도록 합니다.

--- src/main/java/site/moamoa/backend/web/controller/RootController.java
+++ src/main/java/site/moamoa/backend/web/controller/RootController.java
@@ -1,12 +1,16 @@
 package site.moamoa.backend.web.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController
 public class RootController {
+
     @GetMapping("/health")
     public String healthCheck() {
         return "Good!";
     }
+
 }
