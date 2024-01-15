ee4755f8abf8b5c3096c271861c36c077664d175
kellykhy <kellykhy@naver.com>
2024-01-15 21:44:24 +0900

    :sparkles: Feat:도메인 기능 추가

---
+++ src/main/java/site/moamoa/backend/domain/Category.java
@@ -0,0 +1,25 @@
+package site.moamoa.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import site.moamoa.backend.domain.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Category extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name; //카테고리명
+
+    @OneToMany(mappedBy = "post")
+    private List<Post> postList = new ArrayList<>();
+
+}
