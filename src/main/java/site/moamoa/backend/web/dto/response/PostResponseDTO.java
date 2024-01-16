5cc9e963c84da8e2a9b8a03f6920166f45355c2a
Jmmk <jmk525@gmail.com>
2024-01-16 22:14:31 +0900

    :sparkles: Feat:게시글 목록 조회 DTO

---
+++ src/main/java/site/moamoa/backend/web/dto/response/PostResponseDTO.java
@@ -0,0 +1,23 @@
+package site.moamoa.backend.web.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+public class PostResponseDTO {
+
+    @Builder
+    @Getter
+    @RequiredArgsConstructor
+    public static class GetPosts {
+        Long postId;
+        Integer total;
+        Integer d_day;
+        Integer price;
+        CapacityStatus status;
+        String keyword;
+        String image_url;
+    }
+
+}
