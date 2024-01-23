fd45858109711d7ef86b3302e7dc8c1750bb22b5
shihan00321 <shihan001@naver.com>
2024-01-23 21:29:14 +0900

    :bug: Fix: Jpa Auditing 되도록 수정

--- src/main/java/site/moamoa/backend/domain/common/BaseEntity.java
+++ src/main/java/site/moamoa/backend/domain/common/BaseEntity.java
@@ -2,6 +2,8 @@ package site.moamoa.backend.domain.common;
 
 import jakarta.persistence.EntityListeners;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -9,6 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @EntityListeners(AuditingEntityListener.class)
 @Getter
+@MappedSuperclass
 public abstract class BaseEntity {
     @CreatedDate
     private LocalDateTime createdAt;
