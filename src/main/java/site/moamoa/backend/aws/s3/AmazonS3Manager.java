5f8b1b069fc5fc7c21f84fd131ad10db8b3e9941
Jmmk <jmk525@gmail.com>
2024-01-24 15:53:50 +0900

    :sparkles: Feat: S3 이미지 업로드 메서드 추가

--- src/main/java/site/moamoa/backend/aws/s3/AmazonS3Manager.java
+++ src/main/java/site/moamoa/backend/aws/s3/AmazonS3Manager.java
@@ -1,6 +1,7 @@
 package site.moamoa.backend.aws.s3;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
@@ -30,6 +31,22 @@ public class AmazonS3Manager {
             log.error("파일을 업로드하는데 오류가 발생했습니다. : {}", (Object) e.getStackTrace());
         }
         return amazonS3.getUrl(amazonConfig.getBucket(), keyName).toString();
+    }
+
+    public String uploadImage(final String keyName, final MultipartFile file) {
+        String originName = file.getOriginalFilename();
+        String ext = originName.substring(originName.lastIndexOf(".")); //확장자
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType()); //다운로드가 아닌 브라우저로 조회를 하기 위함
+        try{
+            amazonS3.putObject(new PutObjectRequest(amazonConfig.getBucket(), keyName + ext, file.getInputStream(), metadata)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            log.error("파일을 업로드하는데 오류가 발생했습니다. : {}", (Object) e.getStackTrace());
+        }
+        return amazonS3.getUrl(amazonConfig.getBucket(), keyName + ext).toString();
     }
 
     public String generateMemberProfileKeyName() {
