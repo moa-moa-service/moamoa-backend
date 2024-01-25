acaf8ded96b51e675e901624c5b8bfceb56d2232
Jmmk <jmk525@gmail.com>
2024-01-26 00:39:19 +0900

    :sparkles: Feat: 게시물 정원 상태를 만원으로 바꾸는 서비스 로직 구현

--- src/main/java/site/moamoa/backend/web/service/PostCommandService.java
+++ src/main/java/site/moamoa/backend/web/service/PostCommandService.java
@@ -5,7 +5,10 @@ import org.springframework.web.multipart.MultipartFile;
 import site.moamoa.backend.web.dto.base.AuthInfoDTO;
 import site.moamoa.backend.web.dto.request.PostRequestDTO.AddPost;
 import site.moamoa.backend.web.dto.response.PostResponseDTO.AddPostResult;
+import site.moamoa.backend.web.dto.response.PostResponseDTO.UpdatePostStatusResult;
 
 public interface PostCommandService {
     AddPostResult registerPost(AuthInfoDTO auth, AddPost addPost, List<MultipartFile> images);
+
+    UpdatePostStatusResult updatePostStatus(AuthInfoDTO auth, Long postId);
 }
--- src/main/java/site/moamoa/backend/web/service/PostCommandServiceImpl.java
+++ src/main/java/site/moamoa/backend/web/service/PostCommandServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 import site.moamoa.backend.domain.Category;
 import site.moamoa.backend.domain.Member;
 import site.moamoa.backend.domain.Post;
+import site.moamoa.backend.domain.enums.CapacityStatus;
 import site.moamoa.backend.domain.mapping.MemberPost;
 import site.moamoa.backend.domain.mapping.PostImage;
 import site.moamoa.backend.web.converter.MemberPostConverter;
@@ -15,6 +16,8 @@ import site.moamoa.backend.web.converter.PostImageConverter;
 import site.moamoa.backend.web.dto.base.AuthInfoDTO;
 import site.moamoa.backend.web.dto.request.PostRequestDTO.AddPost;
 import site.moamoa.backend.web.dto.response.PostResponseDTO.AddPostResult;
+import site.moamoa.backend.web.dto.response.PostResponseDTO.UpdatePostStatusResult;
+import site.moamoa.backend.web.exception.PostNotFoundException;
 import site.moamoa.backend.web.repository.PostRepository;
 
 @Service
@@ -25,6 +28,7 @@ public class PostCommandServiceImpl implements PostCommandService {
     private final PostImageConverter postImageConverter;
     private final MemberQueryService memberQueryService;
     private final MemberPostCommandService memberPostCommandService;
+    private final MemberPostQueryService memberPostQueryService;
 
     @Override
     public AddPostResult registerPost(AuthInfoDTO auth, AddPost addPost, List<MultipartFile> images) {
@@ -36,8 +40,25 @@ public class PostCommandServiceImpl implements PostCommandService {
         postRepository.save(newPost);
         Member authMember = memberQueryService.findMemberById(auth.id());
         MemberPost newMemberPost = MemberPostConverter.toMemberPost(newPost, authMember);
-        AddPostResult result = PostConverter.toAddPostResult(newPost);
         memberPostCommandService.save(newMemberPost);
-        return result;
+        return PostConverter.toAddPostResult(newPost);
+    }
+
+    @Override
+    public UpdatePostStatusResult updatePostStatus(AuthInfoDTO auth, Long postId) {
+        memberPostQueryService.checkAuthor(auth.id(), postId);
+        updatePostStatusToFull(postId);
+        Post updatedPost = findPostById(postId);
+        return PostConverter.toUpdatePostStatusResult(updatedPost);
+    }
+
+    public Post findPostById(Long postId) {
+        return postRepository.findById(postId).orElseThrow(
+                () -> new PostNotFoundException()
+            );
+    }
+
+    private void updatePostStatusToFull(Long postId) {
+        postRepository.updateStatusToFull(postId, CapacityStatus.NOT_FULL, CapacityStatus.FULL);
     }
 }
