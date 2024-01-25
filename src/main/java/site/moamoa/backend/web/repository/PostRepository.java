bbc72883d0abe2a10262e2ef3727a55bdc960e22
Jmmk <jmk525@gmail.com>
2024-01-26 00:38:38 +0900

    :sparkles: Feat: 게시물 정원 상태를 만원으로 바꾸는 쿼리 구현

--- src/main/java/site/moamoa/backend/web/repository/PostRepository.java
+++ src/main/java/site/moamoa/backend/web/repository/PostRepository.java
@@ -1,8 +1,16 @@
 package site.moamoa.backend.web.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import site.moamoa.backend.domain.Post;
+import site.moamoa.backend.domain.enums.CapacityStatus;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+  @Transactional
+  @Modifying
+  @Query("UPDATE Post mm SET mm.capacityStatus = :afterStatus WHERE mm.id = :postId AND mm.capacityStatus = :previousStatus")
+  void updateStatusToFull(Long postId, CapacityStatus previousStatus, CapacityStatus afterStatus);
 }
