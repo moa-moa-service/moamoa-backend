5838caa98ed4870ee47f949677c989b75d3cda91
Luke_YS <yjsmk0902@gmail.com>
2024-01-26 20:29:16 +0900

    :recycle: Refactor: 개발 완료 API 명세 변경 및 리팩토링

--- src/main/java/site/moamoa/backend/web/controller/PostController.java
+++ src/main/java/site/moamoa/backend/web/controller/PostController.java
@@ -8,7 +8,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Positive;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -96,7 +98,7 @@ public class PostController {
 
     @PostMapping(value = "/api/posts", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(
-            summary = "공동구매 등록 (개발중)",
+            summary = "공동구매 등록",
             description = "공동구매 정보를 받아 새로운 공동구매 게시글을 등록합니다."
     )
     @ApiResponses(value = {
@@ -106,14 +108,14 @@ public class PostController {
             @AuthenticationPrincipal AuthInfoDTO auth,
             @RequestPart("request") AddPost request,
             @RequestPart("files") List<MultipartFile> images
-            ) {
+    ) {
         AddPostResult resultDTO = postCommandService.registerPost(auth, request, images);
         return ApiResponseDTO.onSuccess(resultDTO);
     }
 
     @PatchMapping("/api/posts/{postId}")
     @Operation(
-            summary = "공동구매 상세정보 수정 (개발중)",
+            summary = "공동구매 상세정보 수정",
             description = "공동구매 정보를 받아 기존의 공동구매 게시글을 수정합니다."
     )
     @ApiResponses(value = {
@@ -134,7 +136,7 @@ public class PostController {
 
     @PatchMapping("/api/posts/{postId}/status")
     @Operation(
-            summary = "공동구매 상태 변경 (개발중)",
+            summary = "공동구매 상태 변경",
             description = "기존의 공동구매 상태를 변경합니다."
     )
     @ApiResponses(value = {
@@ -174,7 +176,7 @@ public class PostController {
 
     @PostMapping("/api/posts/{postId}/join")
     @Operation(
-            summary = "공동구매 참여 (개발중)",
+            summary = "공동구매 참여",
             description = "기존의 공동구매에 참여합니다."
     )
     @ApiResponses(value = {
@@ -193,26 +195,26 @@ public class PostController {
 
     @GetMapping("/api/posts")
     @Operation(
-            summary = "특정 키워드를 포함하는 공동구매 조회 (개발중)",
+            summary = "특정 키워드를 포함하는 공동구매 조회",
             description = "특정 키워드가 포함된 공동구매 게시글 리스트를 조회합니다."
     )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "COMMON200", description = "성공입니다.")
     })
     public ApiResponseDTO<GetPosts> searchPostsByKeyword(
-                    @AuthenticationPrincipal AuthInfoDTO auth,
+            @AuthenticationPrincipal AuthInfoDTO auth,
             @Parameter(description = "검색어", example = "사과")
-            @RequestParam(value="keyword") final String keyword,
+            @RequestParam(value = "keyword", required = false) final String keyword,
             @Parameter(description = "카테고리", example = "식품")
-            @RequestParam(value = "category") final String category,
+            @RequestParam(value = "category", required = false) final String category,
             @Parameter(description = "모집까지 남은 일수", example = "4")
-            @RequestParam(value = "dDay") final Integer dDay,
+            @RequestParam(value = "dDay", required = false) final Integer dDay,
             @Parameter(description = "전체 모집 인원", example = "5")
-            @RequestParam(value = "total") final Integer total,
+            @RequestParam(value = "total", required = false) final Integer total,
             @Parameter(description = "최소 금액", example = "3000")
-            @RequestParam(value = "minPrice") final Integer minPrice,
+            @RequestParam(value = "minPrice", required = false) final Integer minPrice,
             @Parameter(description = "최대 금액", example = "5000")
-            @RequestParam(value = "maxPrice") final Integer maxPrice
+            @RequestParam(value = "maxPrice", required = false) final Integer maxPrice
     ) {
         if (!keyword.isEmpty()) {
             postCommandService.updateKeywordCount(auth.id(), keyword);
