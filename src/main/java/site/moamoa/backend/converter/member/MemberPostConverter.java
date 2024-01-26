0508c043e6967bdea1feb8354fd94d6d6bd7980a
Jmmk <jmk525@gmail.com>
2024-01-26 16:19:47 +0900

    :sparkles: Feat: 참여자로 멤버-게시물을 만드는 메서드, 공동구매 참여 응답 DTO 변환 메서드 구현

--- src/main/java/site/moamoa/backend/converter/member/MemberPostConverter.java
+++ src/main/java/site/moamoa/backend/converter/member/MemberPostConverter.java
@@ -4,13 +4,29 @@ import site.moamoa.backend.domain.Member;
 import site.moamoa.backend.domain.Post;
 import site.moamoa.backend.domain.enums.IsAuthorStatus;
 import site.moamoa.backend.domain.mapping.MemberPost;
+import site.moamoa.backend.web.dto.response.PostResponseDTO.AddMemberPostResult;
 
 public class MemberPostConverter {
-  public static MemberPost toMemberPost(Post post, Member member){
+  public static MemberPost toMemberPostAsAuthor(Post post, Member member) {
     return MemberPost.builder()
         .post(post)
         .member(member)
         .isAuthorStatus(IsAuthorStatus.AUTHOR)
+        .build();
+  }
+
+  public static MemberPost toMemberPostAsParticipator(Post post, Member member) {
+    return MemberPost.builder()
+        .post(post)
+        .member(member)
+        .isAuthorStatus(IsAuthorStatus.PARTICIPATOR)
+        .build();
+  }
+
+  public static AddMemberPostResult toAddMemberPostResult(MemberPost memberPost){
+    return AddMemberPostResult.builder()
+        .memberPostId(memberPost.getId())
+        .createdAt(memberPost.getCreatedAt())
         .build();
   }
 }
