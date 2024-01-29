8b5abc36aeb5a078cf26e6f25af59cdfa5d9518b
kellykhy <kellykhy@naver.com>
2024-01-29 18:22:10 +0900

    :recycle: Refactor: BackendApplication 임의로 추가했던 어노테이션 제거

--- src/main/java/site/moamoa/backend/BackendApplication.java
+++ src/main/java/site/moamoa/backend/BackendApplication.java
@@ -8,9 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@OpenAPIDefinition(servers = {
-		@Server(url = "/", description = "Default Server URL")
-})
+
 public class BackendApplication {
 
 	public static void main(String[] args) {
