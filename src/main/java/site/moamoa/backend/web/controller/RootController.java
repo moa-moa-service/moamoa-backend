060e7989f0ac6cecdb7cd284453e4fa4125b404b
Luke_YS <yjsmk0902@gmail.com>
2024-02-06 03:15:52 +0900

    :ambulance: Hotfix: Redis Health Check 추가 (#30)

--- src/main/java/site/moamoa/backend/web/controller/RootController.java
+++ src/main/java/site/moamoa/backend/web/controller/RootController.java
@@ -1,15 +1,21 @@
 package site.moamoa.backend.web.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.moamoa.backend.service.module.redis.RedisModuleService;
 
 @Hidden
 @RestController
+@RequiredArgsConstructor
 public class RootController {
+
+    private final RedisModuleService redisModuleService;
 
     @GetMapping("/health")
     public String healthCheck() {
+        redisModuleService.checkHealth();
         return "Good!";
     }
 
