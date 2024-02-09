3aa012e33610d0ffc65401f5746a22903a2d5c21
kellykhy <kellykhy@naver.com>
2024-02-09 19:21:37 +0900

    :recycle: Refactor: interface 내부 접근제한자 제거 #51

--- src/main/java/site/moamoa/backend/service/component/command/notice/NoticeCommandService.java
+++ src/main/java/site/moamoa/backend/service/component/command/notice/NoticeCommandService.java
@@ -4,9 +4,7 @@ import org.springframework.web.multipart.MultipartFile;
 import site.moamoa.backend.web.dto.request.NoticeRequestDTO.AddNotice;
 import site.moamoa.backend.web.dto.response.NoticeResponseDTO.AddNoticeResult;
 
-import java.util.List;
-
 public interface NoticeCommandService {
 
-    public AddNoticeResult registerNotice(Long memberId, Long postId, AddNotice addNotice, MultipartFile image);
+    AddNoticeResult registerNotice(Long memberId, Long postId, AddNotice addNotice, MultipartFile image);
 }
