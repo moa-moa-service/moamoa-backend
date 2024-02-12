d122a64e4f54ac552d998906bef1c8df8c414573
Luke_YS <yjsmk0902@gmail.com>
2024-02-12 16:00:50 +0900

    :sparkles: Feat: 공동구매 공지사항 댓글 등록 구현 (#52)
    - 공지사항 댓글/대댓글 테스트 완료
    - Comment 관련 예외처리
    - CommentDTO 수정에 따른 CommentConverter 수정

--- src/main/java/site/moamoa/backend/service/module/comment/CommentModuleService.java
+++ src/main/java/site/moamoa/backend/service/module/comment/CommentModuleService.java
@@ -1,4 +1,9 @@
 package site.moamoa.backend.service.module.comment;
 
+import site.moamoa.backend.domain.Comment;
+
 public interface CommentModuleService {
+    Comment findCommentById(Long parentId);
+
+    Comment saveComment(Comment comment);
 }
--- src/main/java/site/moamoa/backend/service/module/comment/CommentModuleServiceImpl.java
+++ src/main/java/site/moamoa/backend/service/module/comment/CommentModuleServiceImpl.java
@@ -2,6 +2,9 @@ package site.moamoa.backend.service.module.comment;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import site.moamoa.backend.api_payload.code.status.ErrorStatus;
+import site.moamoa.backend.api_payload.exception.handler.CommentHandler;
+import site.moamoa.backend.domain.Comment;
 import site.moamoa.backend.repository.comment.CommentRepository;
 
 @Service
@@ -9,4 +12,15 @@ import site.moamoa.backend.repository.comment.CommentRepository;
 public class CommentModuleServiceImpl implements CommentModuleService {
 
     private final CommentRepository commentRepository;
+
+    @Override
+    public Comment findCommentById(Long parentId) {
+        return commentRepository.findById(parentId)
+                .orElseThrow(() -> new CommentHandler(ErrorStatus.COMMENT_NOT_FOUND));
+    }
+
+    @Override
+    public Comment saveComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
 }
